b53e7b4afe1ffcc464d8736579114200e83b476c
JayMil <jmilder.lp@gmail.com>
2021-02-28 14:35:21 -0600

    Add user interface to level 2 and template level

--- platformer/src/UserInterface/UserInterface.gd
+++ platformer/src/UserInterface/UserInterface.gd
@@ -2,14 +2,29 @@ extends Control
 
 onready var scene_tree: = get_tree()
 onready var pause_overlay: ColorRect = get_node("PauseOverlay")
+onready var score: Label = get_node("Label")
+onready var pause_title: Label = get_node("PauseOverlay/Title")
 
 var paused: = false setget set_paused
+
+func _ready():
+	PlayerData.connect("score_updated", self, "update_interface")
+	PlayerData.connect("player_died", self, "_on_PlayerData_player_died")
+	update_interface()
+	
+func _on_PlayerData_player_died():
+	self.paused = true
+	pause_title.text = "You died"
+	
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		self.paused = not paused
 		scene_tree.set_input_as_handled()
-		
+
+func update_interface():
+	score.text = "Score: %s" % PlayerData.score
+	
 	
 func set_paused(value: bool):
 	paused = value
